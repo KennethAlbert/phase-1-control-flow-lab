e36ce5663d7eda0ad8a8f2017e6c1d043a7503e1
Kenneth Albert <gichuka@gmail.com>
2022-06-08 21:58:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let message;
+  if(distance<=400){
+    return message='This one is on me!';
+  }
+ else if(distance>2000 && distance<=2500){
+   return message='I will gladly take your thirty bucks.'
+ }
+ else if (distance>2500)
+   return message='No can do.'
 }
 
-function ternaryCheckCity(){
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+return city==="NYC" ? "Ok, sounds good.": "No go.";
+
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(value){
   // Write your code here!
+  switch (value) {
+    case 'generous':
+        value = 'Thank you so much.';
+        break;
+    case 'not as generous':
+        value = 'Thank you.';
+        break;
+    case 'thanks for everything':
+          value = 'Bye.';
+    }
+    return value;
 }
